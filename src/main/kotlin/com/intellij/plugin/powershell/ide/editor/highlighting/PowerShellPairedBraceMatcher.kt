af0787ff39c03cac3803ae3eadc98b9b8eef2a55
Andrey Dernov <andrey.dernov@jetbrains.com>
2017-11-12 19:12:30 +0300

    brace matcher

---
+++ src/main/kotlin/com/intellij/plugin/powershell/ide/editor/highlighting/PowerShellPairedBraceMatcher.kt
@@ -0,0 +1,31 @@
+package com.intellij.plugin.powershell.ide.editor.highlighting
+
+import com.intellij.codeInsight.highlighting.PairedBraceMatcherAdapter
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.plugin.powershell.lang.PowerShellLanguage
+import com.intellij.plugin.powershell.psi.PowerShellTypes.*
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+
+class PowerShellPairedBraceMatcher : PairedBraceMatcherAdapter(MyPairedBraceMatcher(), PowerShellLanguage.INSTANCE) {
+
+  private class MyPairedBraceMatcher : PairedBraceMatcher {
+    private val PAIRS = arrayOf(BracePair(
+        LCURLY, RCURLY, true),
+        BracePair(LP, RP, false),
+        BracePair(SQBR_L, SQBR_R, false))
+
+    override fun getPairs(): Array<BracePair> {
+      return PAIRS
+    }
+
+    override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?): Boolean {
+      return true
+    }
+
+    override fun getCodeConstructStart(file: PsiFile, openingBraceOffset: Int): Int {
+      return openingBraceOffset
+    }
+  }
+}
