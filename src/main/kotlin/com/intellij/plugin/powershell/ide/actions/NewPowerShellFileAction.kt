9fc9097fa4e2e095707a46194d028615a74144d6
Andrey Dernov <andrey.dernov@jetbrains.com>
2017-11-25 20:37:59 +0300

    create file action

---
+++ src/main/kotlin/com/intellij/plugin/powershell/ide/actions/NewPowerShellFileAction.kt
@@ -0,0 +1,19 @@
+package com.intellij.plugin.powershell.ide.actions
+
+import com.intellij.ide.actions.CreateFileFromTemplateAction
+import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.plugin.powershell.PowerShellIcons
+import com.intellij.psi.PsiDirectory
+
+class NewPowerShellFileAction : CreateFileFromTemplateAction("PowerShell script",
+                                                             "Creates a PowerShell script file from template",
+                                                             PowerShellIcons.FILE), DumbAware {
+  override fun getActionName(directory: PsiDirectory?, newName: String?, templateName: String?): String =
+      "New PowerShell script" + newName
+
+  override fun buildDialog(project: Project?, directory: PsiDirectory?, builder: CreateFileFromTemplateDialog.Builder?) {
+    builder?.setTitle("New PowerShell file")?.addKind("PowerShell file", PowerShellIcons.FILE, "PowerShell File.ps1")
+  }
+}
