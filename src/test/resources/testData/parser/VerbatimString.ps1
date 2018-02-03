e645dbb3e0ecc53e905141b3a25636e00b1e399c
Andrey Dernov <andrey.dernov@jetbrains.com>
2018-02-03 22:13:53 +0300

    verbatim string literal content fixed

---
+++ src/test/resources/testData/parser/VerbatimString.ps1
@@ -0,0 +1,16 @@
+# make a jQuery call
+ExecJavaScript $ie @'
+    // this is JS code, remember to use semicolons
+    var content = $('#home-content');
+    return content.text();
+'@
+@'
+'@
+@'
+'@
+@'
+test text
+'@
+@' wrong
+ss
+'@
