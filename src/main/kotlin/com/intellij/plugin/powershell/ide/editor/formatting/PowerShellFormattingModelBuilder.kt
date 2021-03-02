c9875c1217687b7684e963512e7a4965f683d9b1
Andrey Dernov <andrey.dernov@jetbrains.com>
2021-03-02 21:07:21 +0300

    2021.1 version compatibility

--- src/main/kotlin/com/intellij/plugin/powershell/ide/editor/formatting/PowerShellFormattingModelBuilder.kt
+++ src/main/kotlin/com/intellij/plugin/powershell/ide/editor/formatting/PowerShellFormattingModelBuilder.kt
@@ -1,15 +1,13 @@
 package com.intellij.plugin.powershell.ide.editor.formatting
 
 import com.intellij.formatting.Block
+import com.intellij.formatting.FormattingContext
 import com.intellij.formatting.FormattingModel
 import com.intellij.formatting.FormattingModelBuilder
 import com.intellij.lang.ASTNode
 import com.intellij.openapi.util.TextRange
-import com.intellij.plugin.powershell.lang.PowerShellLanguage
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.TokenType
-import com.intellij.psi.codeStyle.CodeStyleSettings
 import com.intellij.psi.formatter.FormatterUtil
 import com.intellij.psi.formatter.FormattingDocumentModelImpl
 import com.intellij.psi.formatter.PsiBasedFormattingModel
@@ -23,13 +21,14 @@ class PowerShellFormattingModelBuilder : FormattingModelBuilder {
     return elementAtOffset?.textRange
   }
 
-  override fun createModel(element: PsiElement?, settings: CodeStyleSettings): FormattingModel {
-    val containingFile = element?.containingFile?.viewProvider?.getPsi(PowerShellLanguage.INSTANCE) ?: error(element!!.containingFile)
+  override fun createModel(formattingContext: FormattingContext): FormattingModel {
+    val containingFile = formattingContext.containingFile
+    val settings = formattingContext.codeStyleSettings
     val rootBlock = PowerShellBlockImpl(containingFile, settings)
     return PowerShellFormattingModel(containingFile, rootBlock, FormattingDocumentModelImpl.createOn(containingFile))
   }
 
-  private class PowerShellFormattingModel internal constructor(file: PsiFile, rootBlock: Block, documentModel: FormattingDocumentModelImpl)
+  private class PowerShellFormattingModel(file: PsiFile, rootBlock: Block, documentModel: FormattingDocumentModelImpl)
     : PsiBasedFormattingModel(file, rootBlock, documentModel) {
 
 
