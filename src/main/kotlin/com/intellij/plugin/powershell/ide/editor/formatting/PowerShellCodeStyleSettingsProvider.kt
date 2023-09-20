7a2aea5da70bc3a10e29265c1cf49a51e1d07d73
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2023-09-20 16:21:00 +0200

    Fix deprecated API usage: `CodeStyleSettingsProvider.createSettingsPage`

--- src/main/kotlin/com/intellij/plugin/powershell/ide/editor/formatting/PowerShellCodeStyleSettingsProvider.kt
+++ src/main/kotlin/com/intellij/plugin/powershell/ide/editor/formatting/PowerShellCodeStyleSettingsProvider.kt
@@ -1,6 +1,6 @@
 package com.intellij.plugin.powershell.ide.editor.formatting
 
-import com.intellij.openapi.options.Configurable
+import com.intellij.psi.codeStyle.CodeStyleConfigurable
 import com.intellij.psi.codeStyle.CodeStyleSettings
 import com.intellij.psi.codeStyle.CodeStyleSettingsProvider
 import com.intellij.psi.codeStyle.CustomCodeStyleSettings
@@ -14,8 +14,8 @@ class PowerShellCodeStyleSettingsProvider : CodeStyleSettingsProvider() {
     return "PowerShell"
   }
 
-  override fun createSettingsPage(settings: CodeStyleSettings, originalSettings: CodeStyleSettings): Configurable {
-    return PowerShellCodeStyleConfigurable(settings, originalSettings)
+  override fun createConfigurable(settings: CodeStyleSettings, modelSettings: CodeStyleSettings): CodeStyleConfigurable {
+    return PowerShellCodeStyleConfigurable(settings, modelSettings)
   }
 
   override fun createCustomSettings(settings: CodeStyleSettings): CustomCodeStyleSettings {
