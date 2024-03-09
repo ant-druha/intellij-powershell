38d679f249d785f39af70ff4515549b76943d4e8
Friedrich von Never <friedrich@fornever.me>
2024-03-09 21:59:56 +0100

    CI: reformat the publication script

--- src/scripts/Publish-Distribution.ps1
+++ src/scripts/Publish-Distribution.ps1
@@ -1,23 +1,23 @@
 <#
-    .SYNOPSIS
-        This script publishes the plugin distribution from the $DistributionsLocation to the JetBrains Marketplace.
-    .PARAMETER SourceRootPath
-        Path to the project source root.
-    .PARAMETER DistributionsPath
-        Path to the directory containing compressed plugin distribution.
-    .PARAMETER PluginXmlId
-        Plugin identifier.
-    .PARAMETER Channel
-        Channel name to publish the plugin.
-    .PARAMETER AuthToken
-        Token to authenticate to the Marketplace.
+  .SYNOPSIS
+    This script publishes the plugin distribution from the $DistributionsLocation to the JetBrains Marketplace.
+  .PARAMETER SourceRootPath
+    Path to the project source root.
+  .PARAMETER DistributionsPath
+    Path to the directory containing compressed plugin distribution.
+  .PARAMETER PluginXmlId
+    Plugin identifier.
+  .PARAMETER Channel
+    Channel name to publish the plugin.
+  .PARAMETER AuthToken
+    Token to authenticate to the Marketplace.
 #>
 param (
-    [string] $SourceRootPath = "$PSScriptRoot/../..",
-    [string] $DistributionsPath = "$SourceRootPath/build/distributions",
-    [string] $PluginXmlId = 'com.intellij.plugin.adernov.powershell',
-    [Parameter(Mandatory = $true)]
-    [string] $AuthToken
+  [string] $SourceRootPath = "$PSScriptRoot/../..",
+  [string] $DistributionsPath = "$SourceRootPath/build/distributions",
+  [string] $PluginXmlId = 'com.intellij.plugin.adernov.powershell',
+  [Parameter(Mandatory = $true)]
+  [string] $AuthToken
 )
 
 Set-StrictMode -Version Latest
@@ -25,12 +25,12 @@ $ErrorActionPreference = 'Stop'
 
 $file = & "$PSScriptRoot/Get-Distribution.ps1" -DistributionsPath $DistributionsPath
 curl -i `
-    --fail-with-body `
-    --header "Authorization: Bearer $AuthToken" `
-    -F xmlId=$PluginXmlId `
-    -F file=@$file `
-    https://plugins.jetbrains.com/plugin/uploadPlugin
+  --fail-with-body `
+  --header "Authorization: Bearer $AuthToken" `
+  -F xmlId=$PluginXmlId `
+  -F file=@$file `
+  https://plugins.jetbrains.com/plugin/uploadPlugin
 
 if (!$?) {
-   throw "Curl failed with exit code $LASTEXITCODE"
+ throw "Curl failed with exit code $LASTEXITCODE"
 }
