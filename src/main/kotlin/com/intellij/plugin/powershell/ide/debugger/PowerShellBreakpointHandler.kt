adf9b22eeaaf33ca363f3d8a8f635c2989d8fb1e
Phantom <P_hanto_m@outlook.com>
2024-08-02 18:49:02 +0400

    Refactoring WIP 4 Format and cleanup

--- src/main/kotlin/com/intellij/plugin/powershell/ide/debugger/PowerShellBreakpointHandler.kt
+++ src/main/kotlin/com/intellij/plugin/powershell/ide/debugger/PowerShellBreakpointHandler.kt
@@ -1,19 +1,19 @@
 package com.intellij.plugin.powershell.ide.debugger
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.MessageType
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.xdebugger.XDebugSession
 import com.intellij.xdebugger.breakpoints.XBreakpointHandler
 import com.intellij.xdebugger.breakpoints.XBreakpointProperties
 import com.intellij.xdebugger.breakpoints.XBreakpointType
 import com.intellij.xdebugger.breakpoints.XLineBreakpoint
 import com.jetbrains.rd.util.reactive.Signal
 
-class PowerShellBreakpointHandler(powerShellDebugProcess: PowerShellDebugProcess, breakpointTypeClass: Class<out XBreakpointType<XLineBreakpoint<XBreakpointProperties<*>>, *>>): XBreakpointHandler<XLineBreakpoint<XBreakpointProperties<*>>>(
-  breakpointTypeClass
-) {
+class PowerShellBreakpointHandler(
+  powerShellDebugProcess: PowerShellDebugProcess,
+  breakpointTypeClass: Class<out XBreakpointType<XLineBreakpoint<XBreakpointProperties<*>>, *>>
+) : XBreakpointHandler<XLineBreakpoint<XBreakpointProperties<*>>>(breakpointTypeClass) {
+
   val myPowerShellDebugProcess = powerShellDebugProcess
   val registerBreakpointEvent = Signal<Pair<String, XLineBreakpoint<XBreakpointProperties<*>>>>()
   val unregisterBreakpointEvent = Signal<Pair<String, XLineBreakpoint<XBreakpointProperties<*>>>>()
@@ -24,31 +24,12 @@ class PowerShellBreakpointHandler(powerShellDebugProcess: PowerShellDebugProcess
       return
     }
     val file = sourcePosition.file
-    val project: Project = myPowerShellDebugProcess.session.project
     val fileURL: String = getFileURL(file)
     val lineNumber: Int = breakpoint.line
     if (lineNumber == -1) {
-      //myXsltDebugProcess.getSession().setBreakpointInvalid(breakpoint, "Unsupported breakpoint position")
       return
     }
     registerBreakpointEvent.fire(Pair(fileURL, breakpoint))
-    /*try {
-      val manager: BreakpointManager = myPowerShellDebugProcess.getBreakpointManager()
-      var bp: Breakpoint
-      if ((manager.getBreakpoint(fileURL, lineNumber).also { bp = it }) != null) {
-        bp.setEnabled(true)
-      } else {
-        manager.setBreakpoint(fileURL, lineNumber)
-      }
-    } catch (ignore: DebuggerStoppedException) {
-    } catch (e: VMPausedException) {
-      val session: XDebugSession = myXsltDebugProcess.getSession()
-      session.reportMessage(
-        XsltDebuggerBundle.message("notification.content.target.vm.not.responding.breakpoint.can.not.be.set"),
-        MessageType.ERROR
-      )
-      session.setBreakpointInvalid(breakpoint, "Target VM is not responding. Breakpoint can not be set")
-    }*/
   }
 
   override fun unregisterBreakpoint(breakpoint: XLineBreakpoint<XBreakpointProperties<*>>, temporary: Boolean) {
--- src/main/kotlin/com/intellij/plugin/powershell/ide/debugger/PowerShellDebuggerEditorsProvider.kt
+++ src/main/kotlin/com/intellij/plugin/powershell/ide/debugger/PowerShellDebuggerEditorsProvider.kt
@@ -1,14 +1,11 @@
 package com.intellij.plugin.powershell.ide.debugger
 
-import com.intellij.javascript.debugger.execution.xDebugProcessStarter
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.project.Project
 import com.intellij.plugin.powershell.PowerShellFileType
 import com.intellij.psi.PsiDocumentManager
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFileFactory
-import com.intellij.testFramework.utils.editor.saveToDisk
 import com.intellij.util.LocalTimeCounter
 import com.intellij.xdebugger.XDebugSession
 import com.intellij.xdebugger.XDebuggerManager
@@ -20,7 +17,8 @@ import java.util.concurrent.atomic.AtomicInteger
 
 val documentSessionKey = com.intellij.openapi.util.Key.create<XDebugSession>("DocumentSessionKey")
 
-class PowerShellDebuggerEditorsProvider(val xDebugSession: XDebugSession? = null, val fileName: String = "pwsh"): XDebuggerEditorsProvider() {
+class PowerShellDebuggerEditorsProvider(val xDebugSession: XDebugSession? = null, val fileName: String = "pwsh") :
+  XDebuggerEditorsProvider() {
   private var myFileType: PowerShellFileType = PowerShellFileType.INSTANCE
 
   companion object {
@@ -44,7 +42,6 @@ class PowerShellDebuggerEditorsProvider(val xDebugSession: XDebugSession? = null
         LocalTimeCounter.currentTime(), true
       )
     val document = checkNotNull(PsiDocumentManager.getInstance(project).getDocument(psiFile))
-    //document.saveToDisk()
     val session = xDebugSession ?: XDebuggerManager.getInstance(project).currentSession
     document.putUserData(documentSessionKey, session)
     return document
