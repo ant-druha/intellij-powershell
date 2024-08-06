64684f0d323e59e4f95ac778e90e2631f7d5d4ec
Phantom <P_hanto_m@outlook.com>
2024-08-06 18:05:39 +0400

    Refactoring WIP 7

--- src/main/kotlin/com/intellij/plugin/powershell/ide/debugger/PowerShellBreakpointHandler.kt
+++ src/main/kotlin/com/intellij/plugin/powershell/ide/debugger/PowerShellBreakpointHandler.kt
@@ -1,8 +1,9 @@
 package com.intellij.plugin.powershell.ide.debugger
 
-import com.intellij.openapi.project.Project
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.plugin.powershell.ide.MessagesBundle
 import com.intellij.xdebugger.breakpoints.XBreakpointHandler
 import com.intellij.xdebugger.breakpoints.XBreakpointProperties
 import com.intellij.xdebugger.breakpoints.XBreakpointType
@@ -10,23 +11,26 @@ import com.intellij.xdebugger.breakpoints.XLineBreakpoint
 import com.jetbrains.rd.util.reactive.Signal
 
 class PowerShellBreakpointHandler(
-  powerShellDebugProcess: PowerShellDebugProcess,
+  val powerShellDebugProcess: PowerShellDebugProcess,
   breakpointTypeClass: Class<out XBreakpointType<XLineBreakpoint<XBreakpointProperties<*>>, *>>
 ) : XBreakpointHandler<XLineBreakpoint<XBreakpointProperties<*>>>(breakpointTypeClass) {
 
-  val myPowerShellDebugProcess = powerShellDebugProcess
+  val logger = logger<PowerShellBreakpointHandler>()
   val registerBreakpointEvent = Signal<Pair<String, XLineBreakpoint<XBreakpointProperties<*>>>>()
   val unregisterBreakpointEvent = Signal<Pair<String, XLineBreakpoint<XBreakpointProperties<*>>>>()
 
   override fun registerBreakpoint(breakpoint: XLineBreakpoint<XBreakpointProperties<*>>) {
     val sourcePosition = breakpoint.sourcePosition
     if (sourcePosition == null || !sourcePosition.file.exists() || !sourcePosition.file.isValid) {
+      logger.warn("Invalid breakpoint $breakpoint")
       return
     }
     val file = sourcePosition.file
     val fileURL: String = getFileURL(file)
     val lineNumber: Int = breakpoint.line
     if (lineNumber == -1) {
+      powerShellDebugProcess.xDebugSession.setBreakpointInvalid(breakpoint, MessagesBundle.message("powershell.debugger.breakpoints.invalidBreakPoint"))
+      logger.warn("Invalid breakpoint $breakpoint - line $lineNumber")
       return
     }
     registerBreakpointEvent.fire(Pair(fileURL, breakpoint))
@@ -35,13 +39,14 @@ class PowerShellBreakpointHandler(
   override fun unregisterBreakpoint(breakpoint: XLineBreakpoint<XBreakpointProperties<*>>, temporary: Boolean) {
     val sourcePosition = breakpoint.sourcePosition
     if (sourcePosition == null || !sourcePosition.file.exists() || !sourcePosition.file.isValid) {
+      logger.warn("Invalid breakpoint $breakpoint")
       return
     }
     val file = sourcePosition.file
     val fileURL: String = getFileURL(file)
     val lineNumber: Int = breakpoint.line
     if (lineNumber == -1) {
-      //myXsltDebugProcess.getSession().setBreakpointInvalid(breakpoint, "Unsupported breakpoint position")
+      logger.warn("Invalid breakpoint $breakpoint - line $lineNumber")
       return
     }
     unregisterBreakpointEvent.fire(Pair(fileURL, breakpoint))  }
