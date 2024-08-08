f5a999ec7442ab1a79465e6ff47bac397bc4c53b
Friedrich von Never <friedrich@fornever.me>
2024-08-08 18:51:01 +0200

    (#19) Debugger tests: new run in EDT mode

--- src/test/kotlin/com/intellij/plugin/powershell/debugger/EvaluationTest.kt
+++ src/test/kotlin/com/intellij/plugin/powershell/debugger/EvaluationTest.kt
@@ -10,31 +10,32 @@ import junit.framework.TestCase
 class EvaluationTest: DebuggerTestBase() {
 
   fun testEvaluation() {
-    val psiFile = copyAndOpenFile("debugger/testBreakpoint.ps1")
-    val file = psiFile.virtualFile
+    runInEdt {
+      val psiFile = copyAndOpenFile("debugger/testBreakpoint.ps1")
+      val file = psiFile.virtualFile
 
-    val fileLine = 1 // line in file, starting from 1
-    val line = fileLine - 1 // breakpoint line, starting from 0
-    val expression = "1 + 2"
-    val expectedResult = "3"
+      val fileLine = 1 // line in file, starting from 1
+      val line = fileLine - 1 // breakpoint line, starting from 0
+      val expression = "1 + 2"
+      val expectedResult = "3"
 
-    val testSession = PowerShellTestSession(project, file.toNioPath())
-    XDebuggerTestUtil.toggleBreakpoint(project, file, line)
-    Lifetime.using { lt ->
-      val debugSession = testSession.startDebugSession(lt)
-      XDebuggerTestUtil.waitFor(
-        testSession.sessionListener.pausedSemaphore,
-        testSession.waitForBackgroundTimeout.toMillis()
-      )
-      val variableValue =
-        XDebuggerTestUtil.evaluate(debugSession, expression, testSession.waitForBackgroundTimeout.toMillis()).first
-      val variableValueNode = XDebuggerTestUtil.computePresentation(variableValue)
-      TestCase.assertEquals(expectedResult, variableValueNode.myValue)
+      val testSession = PowerShellTestSession(project, file.toNioPath())
+      XDebuggerTestUtil.toggleBreakpoint(project, file, line)
+      Lifetime.using { lt ->
+        val debugSession = testSession.startDebugSession(lt)
+        XDebuggerTestUtil.waitFor(
+          testSession.sessionListener.pausedSemaphore,
+          testSession.waitForBackgroundTimeout.toMillis()
+        )
+        val variableValue =
+          XDebuggerTestUtil.evaluate(debugSession, expression, testSession.waitForBackgroundTimeout.toMillis()).first
+        val variableValueNode = XDebuggerTestUtil.computePresentation(variableValue)
+        TestCase.assertEquals(expectedResult, variableValueNode.myValue)
+      }
     }
   }
 
-  override fun tearDown() {
+  override fun tearDownEdt() {
     FileEditorManagerEx.getInstanceEx(project).closeAllFiles()
-    super.tearDown()
   }
 }
--- src/test/kotlin/com/intellij/plugin/powershell/debugger/StepTest.kt
+++ src/test/kotlin/com/intellij/plugin/powershell/debugger/StepTest.kt
@@ -10,103 +10,105 @@ import junit.framework.TestCase
 
 class StepTest: DebuggerTestBase() {
 
-  fun testStepOver()
-  {
-    val psiFile = copyAndOpenFile("debugger/stepTest.ps1")
-    val file = psiFile.virtualFile
+  fun testStepOver() {
+    runInEdt {
+      val psiFile = copyAndOpenFile("debugger/stepTest.ps1")
+      val file = psiFile.virtualFile
 
-    val fileLine = 11 // line in file, starting from 1
-    val line = fileLine - 1 // breakpoint line, starting from 0
+      val fileLine = 11 // line in file, starting from 1
+      val line = fileLine - 1 // breakpoint line, starting from 0
 
-    val testSession = PowerShellTestSession(project, file.toNioPath())
-    XDebuggerTestUtil.toggleBreakpoint(project, file, line)
-    Lifetime.using { lt ->
-      val debugSession = testSession.startDebugSession(lt)
-      XDebuggerTestUtil.waitFor(
-        testSession.sessionListener.pausedSemaphore,
-        testSession.waitForBackgroundTimeout.toMillis()
-      )
-      val suspendContext = debugSession.suspendContext as PowerShellSuspendContext
-      TestCase.assertEquals(line, suspendContext.activeExecutionStack.topFrame?.sourcePosition?.line)
-      debugSession.stepOver(false)
-      XDebuggerTestUtil.waitFor(
-        testSession.sessionListener.pausedSemaphore,
-        testSession.waitForBackgroundTimeout.toMillis()
-      )
-      TestCase.assertEquals(
-        line + 1,
-        (debugSession.suspendContext as PowerShellSuspendContext).activeExecutionStack.topFrame?.sourcePosition?.line
-      )
+      val testSession = PowerShellTestSession(project, file.toNioPath())
+      XDebuggerTestUtil.toggleBreakpoint(project, file, line)
+      Lifetime.using { lt ->
+        val debugSession = testSession.startDebugSession(lt)
+        XDebuggerTestUtil.waitFor(
+          testSession.sessionListener.pausedSemaphore,
+          testSession.waitForBackgroundTimeout.toMillis()
+        )
+        val suspendContext = debugSession.suspendContext as PowerShellSuspendContext
+        TestCase.assertEquals(line, suspendContext.activeExecutionStack.topFrame?.sourcePosition?.line)
+        debugSession.stepOver(false)
+        XDebuggerTestUtil.waitFor(
+          testSession.sessionListener.pausedSemaphore,
+          testSession.waitForBackgroundTimeout.toMillis()
+        )
+        TestCase.assertEquals(
+          line + 1,
+          (debugSession.suspendContext as PowerShellSuspendContext).activeExecutionStack.topFrame?.sourcePosition?.line
+        )
+      }
     }
   }
 
-  fun testStepIn()
-  {
-    val psiFile = copyAndOpenFile("debugger/stepTest.ps1")
-    val file = psiFile.virtualFile
+  fun testStepIn() {
+    runInEdt {
+      val psiFile = copyAndOpenFile("debugger/stepTest.ps1")
+      val file = psiFile.virtualFile
 
-    val fileLine = 11 // line in file, starting from 1
-    val line = fileLine - 1 // breakpoint line, starting from 0
+      val fileLine = 11 // line in file, starting from 1
+      val line = fileLine - 1 // breakpoint line, starting from 0
 
-    val stepInLine = 0
+      val stepInLine = 0
 
-    val testSession = PowerShellTestSession(project, file.toNioPath())
-    XDebuggerTestUtil.toggleBreakpoint(project, file, line)
-    Lifetime.using { lt ->
-      val debugSession = testSession.startDebugSession(lt)
-      XDebuggerTestUtil.waitFor(
-        testSession.sessionListener.pausedSemaphore,
-        testSession.waitForBackgroundTimeout.toMillis()
-      )
-      val suspendContext = debugSession.suspendContext as PowerShellSuspendContext
-      TestCase.assertEquals(line, suspendContext.activeExecutionStack.topFrame?.sourcePosition?.line)
-      debugSession.stepInto()
-      XDebuggerTestUtil.waitFor(
-        testSession.sessionListener.pausedSemaphore,
-        testSession.waitForBackgroundTimeout.toMillis()
-      )
-      TestCase.assertEquals(
-        stepInLine,
-        (debugSession.suspendContext as PowerShellSuspendContext).activeExecutionStack.topFrame?.sourcePosition?.line
-      )
+      val testSession = PowerShellTestSession(project, file.toNioPath())
+      XDebuggerTestUtil.toggleBreakpoint(project, file, line)
+      Lifetime.using { lt ->
+        val debugSession = testSession.startDebugSession(lt)
+        XDebuggerTestUtil.waitFor(
+          testSession.sessionListener.pausedSemaphore,
+          testSession.waitForBackgroundTimeout.toMillis()
+        )
+        val suspendContext = debugSession.suspendContext as PowerShellSuspendContext
+        TestCase.assertEquals(line, suspendContext.activeExecutionStack.topFrame?.sourcePosition?.line)
+        debugSession.stepInto()
+        XDebuggerTestUtil.waitFor(
+          testSession.sessionListener.pausedSemaphore,
+          testSession.waitForBackgroundTimeout.toMillis()
+        )
+        TestCase.assertEquals(
+          stepInLine,
+          (debugSession.suspendContext as PowerShellSuspendContext).activeExecutionStack.topFrame?.sourcePosition?.line
+        )
+      }
     }
   }
 
-  fun testStepOut()
-  {
-    val psiFile = copyAndOpenFile("debugger/stepTest.ps1")
-    val file = psiFile.virtualFile
+  fun testStepOut() {
+    runInEdt {
+      val psiFile = copyAndOpenFile("debugger/stepTest.ps1")
+      val file = psiFile.virtualFile
 
-    val fileLine = 7 // line in file, starting from 1
-    val line = fileLine - 1 // breakpoint line, starting from 0
+      val fileLine = 7 // line in file, starting from 1
+      val line = fileLine - 1 // breakpoint line, starting from 0
 
-    val stepOutFileLine = 12
-    val stepOutLine = stepOutFileLine - 1
+      val stepOutFileLine = 12
+      val stepOutLine = stepOutFileLine - 1
 
-    val testSession = PowerShellTestSession(project, file.toNioPath())
-    XDebuggerTestUtil.toggleBreakpoint(project, file, line)
-    Lifetime.using { lt ->
-      val debugSession = testSession.startDebugSession(lt)
-      XDebuggerTestUtil.waitFor(
-        testSession.sessionListener.pausedSemaphore,
-        testSession.waitForBackgroundTimeout.toMillis()
-      )
-      val suspendContext = debugSession.suspendContext as PowerShellSuspendContext
-      TestCase.assertEquals(line, suspendContext.activeExecutionStack.topFrame?.sourcePosition?.line)
-      debugSession.stepOut()
-      XDebuggerTestUtil.waitFor(
-        testSession.sessionListener.pausedSemaphore,
-        testSession.waitForBackgroundTimeout.toMillis()
-      )
-      TestCase.assertEquals(
-        stepOutLine,
-        (debugSession.suspendContext as PowerShellSuspendContext).activeExecutionStack.topFrame?.sourcePosition?.line
-      )
+      val testSession = PowerShellTestSession(project, file.toNioPath())
+      XDebuggerTestUtil.toggleBreakpoint(project, file, line)
+      Lifetime.using { lt ->
+        val debugSession = testSession.startDebugSession(lt)
+        XDebuggerTestUtil.waitFor(
+          testSession.sessionListener.pausedSemaphore,
+          testSession.waitForBackgroundTimeout.toMillis()
+        )
+        val suspendContext = debugSession.suspendContext as PowerShellSuspendContext
+        TestCase.assertEquals(line, suspendContext.activeExecutionStack.topFrame?.sourcePosition?.line)
+        debugSession.stepOut()
+        XDebuggerTestUtil.waitFor(
+          testSession.sessionListener.pausedSemaphore,
+          testSession.waitForBackgroundTimeout.toMillis()
+        )
+        TestCase.assertEquals(
+          stepOutLine,
+          (debugSession.suspendContext as PowerShellSuspendContext).activeExecutionStack.topFrame?.sourcePosition?.line
+        )
+      }
     }
   }
 
-  override fun tearDown() {
+  override fun tearDownEdt() {
     FileEditorManagerEx.getInstanceEx(project).closeAllFiles()
-    super.tearDown()
   }
 }
