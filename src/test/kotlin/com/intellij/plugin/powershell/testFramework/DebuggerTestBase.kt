1cc7ac3bc019f479130e8c9fb9c8747f64c46cb8
Friedrich von Never <friedrich@fornever.me>
2025-04-16 22:35:18 +0200

    (#291) DebuggerTestBase: minor cleanup

--- src/test/kotlin/com/intellij/plugin/powershell/testFramework/DebuggerTestBase.kt
+++ src/test/kotlin/com/intellij/plugin/powershell/testFramework/DebuggerTestBase.kt
@@ -1,12 +1,10 @@
 package com.intellij.plugin.powershell.testFramework
 
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.util.ThrowableComputable
 import com.intellij.openapi.vfs.VfsUtil
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
-import com.intellij.util.ThrowableRunnable
-import java.io.IOException
 import kotlin.io.path.Path
 import kotlin.io.path.createDirectories
 
@@ -26,13 +24,9 @@ abstract class DebuggerTestBase: PowerShellTestBase() {
     val baseDir = VfsUtil.findFile(basePath, /* refreshIfNeeded = */ true)
       ?: error("Cannot find the base directory for project $project.")
 
-    var result: VirtualFile? = null
-
-    WriteCommandAction.writeCommandAction(null).run<IOException?>(ThrowableRunnable {
-      result = originalVirtual.copy(originalVirtual, baseDir, originalVirtual.name)
+    val result = WriteCommandAction.writeCommandAction(/* project = */ null).compute(ThrowableComputable {
+      originalVirtual.copy(originalVirtual, baseDir, originalVirtual.name)
     })
-
-    if (result == null) error("Cannot copy the original file \"$originalVirtual\".")
 
     return PsiManager.getInstance(project).findFile(result) ?: error("Cannot find the PSI file for \"$result\".")
   }
